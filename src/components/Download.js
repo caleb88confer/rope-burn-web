76f5a16e1cdb72dc41aee8f57a56015226966c35
Caleb Confer <caleb88confer@gmail.com>
2021-08-03 12:55:35 -0400

    adds download link

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,7 +1,14 @@
+import {Link} from "react-router-dom";
+
 function Download (props) {
     return (
         <div>
             <h1>Download</h1>
+            <a href="public/plugins/RopeBurn.zip" download>Rope Burn Download</a>
+            <br />
+            <a href="public/images/Screen Shot 2021-07-24 at 3.13.18 PM.png" download>picture</a>
+            <br />
+            <Link to="/Users/calebconfer/Desktop/rope burn distortion/rope-burn-web/public/images/Screen Shot 2021-07-24 at 3.13.18 PM.png" target="_blank" download>Picture download</Link>
         </div>
 
     )
